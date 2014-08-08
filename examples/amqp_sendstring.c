13db51da7fa58bb51369a47efd088fda721c8c2b
Fabio Strozzi <fabio.strozzi@mapsgroup.it>
2014-08-08 10:12:50 +0200

    Username and password can be specified as command line options

--- examples/amqp_sendstring.c
+++ examples/amqp_sendstring.c
@@ -52,11 +52,13 @@ int main(int argc, char const *const *argv)
   char const *exchange;
   char const *routingkey;
   char const *messagebody;
+  char const *username;
+  char const *password;
   amqp_socket_t *socket = NULL;
   amqp_connection_state_t conn;
 
   if (argc < 6) {
-    fprintf(stderr, "Usage: amqp_sendstring host port exchange routingkey messagebody\n");
+    fprintf(stderr, "Usage: amqp_sendstring host port exchange routingkey messagebody username password\n");
     return 1;
   }
 
@@ -65,6 +67,8 @@ int main(int argc, char const *const *argv)
   exchange = argv[3];
   routingkey = argv[4];
   messagebody = argv[5];
+  username = argv[6];
+  password = argv[7];
 
   conn = amqp_new_connection();
 
@@ -78,7 +82,7 @@ int main(int argc, char const *const *argv)
     die("opening TCP socket");
   }
 
-  die_on_amqp_error(amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, "guest", "guest"),
+  die_on_amqp_error(amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, username, password),
                     "Logging in");
   amqp_channel_open(conn, 1);
   die_on_amqp_error(amqp_get_rpc_reply(conn), "Opening channel");
